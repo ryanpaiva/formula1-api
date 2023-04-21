c6c37b3a1a6e3546786747e3e54389b89a43da84
Ryan Tulio Paiva <ryantulio001@gmail.com>
2023-04-21 15:58:23 -0300

    first commit

---
+++ src/components/services/positions.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './positions.css'
+
+function Positions() {
+    const [driverStandings, setDriverStandings] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const response = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+        const data = await response.json();
+        setDriverStandings(data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    };
+
+    return (
+        <div className='position-box'>
+            <h1>Classificação de Pilotos:</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Posição</th>
+                        <th>Piloto</th>
+                        <th>Scuderia</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {driverStandings.map((driver) => (
+                        <tr key={driver.Driver.driverId}>
+                            <td>{driver.position}</td>
+                            <td><a target='_blank' href={`${driver.Driver.url}`}>{driver.Driver.givenName} {driver.Driver.familyName}</a></td>
+                            <td>{driver.Constructors[0].name}</td>
+                            <td>{driver.points}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export { Positions }
